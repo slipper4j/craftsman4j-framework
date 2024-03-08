2619d11a55f54e6436f44ffbfbca53c49201dac5
zhougang <921366807@qq.com>
2024-03-08 23:48:21 +0800

    feat: update LockFailureStrategy

--- craftsman4j-spring-boot-starter-lock/src/main/java/com/craftsman4j/framework/lock4j/config/Lock4jConfiguration.java
+++ craftsman4j-spring-boot-starter-lock/src/main/java/com/craftsman4j/framework/lock4j/config/Lock4jConfiguration.java
@@ -1,18 +1,29 @@
 package com.craftsman4j.framework.lock4j.config;
 
-import com.craftsman4j.framework.lock4j.core.DefaultLockFailureStrategy;
+import cn.hutool.extra.spring.SpringUtil;
+import com.baomidou.lock.LockFailureStrategy;
+import com.baomidou.lock.LockTemplate;
 import com.baomidou.lock.spring.boot.autoconfigure.LockAutoConfiguration;
+import com.craftsman4j.framework.lock4j.core.DefaultLockFailureStrategy;
+import com.craftsman4j.framework.lock4j.core.util.Lock4jUtils;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 
 @AutoConfiguration(before = LockAutoConfiguration.class)
 @ConditionalOnClass(name = "com.baomidou.lock.annotation.Lock4j")
-public class Lock4jConfiguration {
+public class Lock4jConfiguration implements InitializingBean {
 
-    @Bean
+    @ConditionalOnMissingBean(LockFailureStrategy.class)
     public DefaultLockFailureStrategy lockFailureStrategy() {
         return new DefaultLockFailureStrategy();
+    }
+
+    @Override
+    public void afterPropertiesSet() {
+        Lock4jUtils.setLockTemplate(SpringUtil.getBean(LockTemplate.class));
     }
 
 }
