2eecb588a9d0e339cebf358434fc892c27a8e833
zhougang <921366807@qq.com>
2024-03-12 23:08:32 +0800

    feat: update

--- craftsman4j-common/src/main/java/com/craftsman4j/framework/common/pojo/PageParam.java
+++ craftsman4j-common/src/main/java/com/craftsman4j/framework/common/pojo/PageParam.java
@@ -3,12 +3,12 @@ package com.craftsman4j.framework.common.pojo;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
-import javax.validation.constraints.Min;
 import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
-@Schema(description="分页参数")
+@Schema(description = "分页参数")
 @Data
 public class PageParam implements Serializable {
 
@@ -17,12 +17,12 @@ public class PageParam implements Serializable {
 
     /**
      * 每页条数 - 不分页
-     *
+     * <p>
      * 例如说，导出接口，可以设置 {@link #pageSize} 为 -1 不分页，查询所有数据。
      */
     public static final Integer PAGE_SIZE_NONE = -1;
 
-    @Schema(description = "页码，从 1 开始", requiredMode = Schema.RequiredMode.REQUIRED,example = "1")
+    @Schema(description = "页码，从 1 开始", requiredMode = Schema.RequiredMode.REQUIRED, example = "1")
     @NotNull(message = "页码不能为空")
     @Min(value = 1, message = "页码最小值为 1")
     private Integer pageNo = PAGE_NO;
@@ -32,8 +32,5 @@ public class PageParam implements Serializable {
     @Min(value = 1, message = "每页条数最小值为 1")
     @Max(value = 100, message = "每页条数最大值为 100")
     private Integer pageSize = PAGE_SIZE;
-
-    @Schema(description = "排序字段", requiredMode = Schema.RequiredMode.REQUIRED, example = "[id asc,create_time desc]")
-    private String[] orders;
 
 }
--- craftsman4j-spring-boot-starter-mybatis/src/main/java/com/craftsman4j/framework/mybatis/core/util/PageHelperUtils.java
+++ craftsman4j-spring-boot-starter-mybatis/src/main/java/com/craftsman4j/framework/mybatis/core/util/PageHelperUtils.java
@@ -1,16 +1,15 @@
 package com.craftsman4j.framework.mybatis.core.util;
 
-import cn.hutool.core.util.ArrayUtil;
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.util.StrUtil;
 import com.craftsman4j.framework.common.pojo.PageParam;
 import com.craftsman4j.framework.common.pojo.PageResult;
+import com.craftsman4j.framework.common.pojo.SortablePageParam;
 import com.craftsman4j.framework.common.pojo.SortingField;
-import com.craftsman4j.framework.common.util.servlet.ServletUtils;
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
-import org.apache.commons.lang3.StringUtils;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
@@ -25,42 +24,28 @@ public class PageHelperUtils {
 
     public static final String ORDERS = "orders";
 
-    public static <T> Page<T> doPage() {
-        return doPage(null);
+    public static <T> Page<T> startPage(PageParam pageParam) {
+        return startPage(pageParam, null);
     }
 
     /**
      * PageHelper分页
      *
-     * @param columnsMap key:前端传过来的字段名，value:表别名.表字段名
-     * @param <T>        <T>
+     * @param pageParam     pageParam
+     * @param orderFieldMap key:前端传过来的字段名，value:表别名.表字段名
+     * @param <T>           <T>
      * @return Page<T>
      */
-    public static <T> Page<T> doPage(Map<String, String> columnsMap) {
-        PageParam pageParam = getPageDTO();
+    public static <T> Page<T> startPage(PageParam pageParam, Map<String, String> orderFieldMap) {
         Page<T> page;
-        if (ArrayUtil.isEmpty(pageParam.getOrders())) {
-            page = PageHelper.startPage(pageParam.getPageNo(), pageParam.getPageSize());
+        SortablePageParam sortablePageParam;
+        if (pageParam instanceof SortablePageParam &&
+                CollUtil.isNotEmpty((sortablePageParam = (SortablePageParam) pageParam).getSortingFields())) {
+            page = PageHelper.startPage(pageParam.getPageNo(), pageParam.getPageSize(), orderBy(sortablePageParam.getSortingFields(), orderFieldMap));
         } else {
-            page = PageHelper.startPage(pageParam.getPageNo(), pageParam.getPageSize(), orderBy(pageParam.getOrders(), columnsMap));
+            page = PageHelper.startPage(pageParam.getPageNo(), pageParam.getPageSize());
         }
         return page;
-    }
-
-    public static PageParam getPageDTO() {
-        HttpServletRequest request = ServletUtils.getRequest();
-        PageParam pageParam = new PageParam();
-        assert request != null;
-        if (StringUtils.isNotBlank(request.getParameter(PAGE_NUM))) {
-            pageParam.setPageNo(Integer.parseInt(request.getParameter(PAGE_NUM)));
-        }
-        if (StringUtils.isNotBlank(request.getParameter(PAGE_SIZE))) {
-            pageParam.setPageSize(Integer.parseInt(request.getParameter(PAGE_SIZE)));
-        }
-        if (request.getParameterValues(ORDERS) != null) {
-            pageParam.setOrders(request.getParameterValues(ORDERS));
-        }
-        return pageParam;
     }
 
     public static <T> PageResult<T> toResult(List<T> pageList) {
@@ -80,50 +65,32 @@ public class PageHelperUtils {
      */
     public static final String FIELD_REGEXP = "([a-zA-Z0-9_]+)";
 
-    private static String orderBy(String[] orders, Map<String, String> columnsMap) {
-        if (ArrayUtil.isEmpty(orders)) {
+    private static String orderBy(List<SortingField> sortingFields, Map<String, String> columnsMap) {
+        if (CollUtil.isEmpty(sortingFields)) {
             return "";
         }
         StringBuilder stringBuilder = new StringBuilder();
-        for (int x = 0; x < orders.length; x++) {
-            String order = orders[x];
-            if (StringUtils.isBlank(order)) {
-                continue;
-            }
-            String[] split = order.split(" ");
-            String column = split[0].trim();
-            String orderType = split[1].trim().toLowerCase();
+        for (int i = 0; i < sortingFields.size(); i++) {
+            SortingField order = sortingFields.get(i);
+            String field = order.getField();
+            String orderType = order.getOrder().toLowerCase();
             if (!(orderType.equals(SortingField.ORDER_ASC) || orderType.equals(SortingField.ORDER_DESC))) {
                 throw new IllegalArgumentException("非法的排序策略：" + orderType);
             }
             // 判断列名称的合法性，防止SQL注入。只能是【字母，数字，下划线】
-            if (!Pattern.matches(FIELD_REGEXP, column)) {
-                throw new IllegalArgumentException("非法的排序字段名称：" + column);
+            if (!Pattern.matches(FIELD_REGEXP, field)) {
+                throw new IllegalArgumentException("非法的排序字段名称：" + field);
             }
-            if (x != 0) {
+            if (i != 0) {
                 stringBuilder.append(", ");
             }
-            if (columnsMap != null && columnsMap.get(column) != null) {
-                stringBuilder.append(columnsMap.get(column));
+            if (columnsMap != null && columnsMap.get(field) != null) {
+                stringBuilder.append(columnsMap.get(field));
             } else {
                 // 驼峰转换为下划线
-                column = humpConversionUnderscore(column);
-                stringBuilder.append("`").append(column).append("`");
+                stringBuilder.append("`").append(StrUtil.toUnderlineCase(field)).append("`");
             }
             stringBuilder.append(" ").append(order);
-        }
-        return stringBuilder.toString();
-    }
-
-    private static String humpConversionUnderscore(String value) {
-        StringBuilder stringBuilder = new StringBuilder();
-        char[] chars = value.toCharArray();
-        for (char character : chars) {
-            if (Character.isUpperCase(character)) {
-                stringBuilder.append("_");
-                character = Character.toLowerCase(character);
-            }
-            stringBuilder.append(character);
         }
         return stringBuilder.toString();
     }
